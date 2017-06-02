d4b1a2ab701d4e33c46c2e35380799f95b68e8f2
Lucas Ventura <lucas.ventura@safelayer.com>
2017-06-02 10:31:31 +0200

    - Switched to a multi-threaded application, faster and more reproducible situation

--- log4j2-rollover/src/main/java/com/github/lucasvc/rsyslog_tests/log4j2_rollover/LoggingRollover.java
+++ log4j2-rollover/src/main/java/com/github/lucasvc/rsyslog_tests/log4j2_rollover/LoggingRollover.java
@@ -8,9 +8,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -21,44 +25,75 @@ public class LoggingRollover {
 	private static final Random random = new Random();
 
 	public static void main(String[] args) throws Exception {
-		Path output = Paths.get("build/logs");
+		final Path output = Paths.get("build/logs");
 		System.setProperty("outputDir", output.toString());
-		String fileName = "json-rollover.log";
+		final String fileName = "json-rollover.log";
 		System.setProperty("fileName", fileName);
-		Logger logger = LogManager.getLogger("services");
-		int i = 0;
-		while (!hasRolled(output, fileName, i, 10)) {
-			Map<String, String> event = new HashMap<>(3);
-			event.put("name", "LoggingRollover");
-			event.put("component", "rsyslog");
-			event.put("trace_id", String.valueOf(i++));
-			event.put(generateRand(32), generateRand(32));
-			event.put(generateRand(32), generateRand(32));
-			event.put(generateRand(32), generateRand(32));
-			event.put(generateRand(32), generateRand(32));
-			event.put(generateRand(32), generateRand(32));
-			event.put(generateRand(32), generateRand(32));
-			event.put(generateRand(32), generateRand(32));
-			event.put(generateRand(32), generateRand(32));
-			event.put(generateRand(32), generateRand(32));
-			event.put(generateRand(32), generateRand(32));
-			event.put(generateRand(32), generateRand(32));
-			logger.info(new MapMessage(event));
-			Thread.sleep(5);
+		final Logger logger = LogManager.getLogger("services");
+		final AtomicInteger trace = new AtomicInteger(0);
+		final AtomicBoolean stop = new AtomicBoolean(false);
+		final int rolls = 5;
+		int threads = 10;
+		ExecutorService executor = Executors.newFixedThreadPool(threads);
+		executor.submit(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					while (!hasRolled(output, fileName, rolls)) {
+						Thread.sleep(200);
+					}
+				}
+				catch (Exception e) {
+					throw new RuntimeException();
+				}
+				finally {
+					stop.set(true);
+				}
+			}
+		});
+		for (int i = 0; i < threads; i++) {
+			executor.submit(new Runnable() {
+				@Override
+				public void run() {
+					while (!stop.get()) {
+						Map<String, String> event = new HashMap<>(3);
+						event.put("name", "LoggingRollover");
+						event.put("component", "rsyslog");
+						event.put("trace_id", String.valueOf(trace.getAndIncrement()));
+						event.put(generateRand(32), generateRand(32));
+						event.put(generateRand(32), generateRand(32));
+						event.put(generateRand(32), generateRand(32));
+						event.put(generateRand(32), generateRand(32));
+						event.put(generateRand(32), generateRand(32));
+						event.put(generateRand(32), generateRand(32));
+						event.put(generateRand(32), generateRand(32));
+						event.put(generateRand(32), generateRand(32));
+						event.put(generateRand(32), generateRand(32));
+						event.put(generateRand(32), generateRand(32));
+						event.put(generateRand(32), generateRand(32));
+						logger.info(new MapMessage(event));
+					}
+				}
+			});
 		}
-		List<String> lines = Files.readAllLines(Paths.get("rsyslog/output/recieved.log"), StandardCharsets.UTF_8);
-		if (i != lines.size())
-			throw new AssertionError("Expected '" + i + "' lines but got '" + lines.size() + "'");
+		executor.shutdown();
+		executor.awaitTermination(10, TimeUnit.MINUTES);
+
+		int expected = trace.get();
+		int actual = -1;
+		do {
+			Thread.sleep(1000);
+			actual = Files.readAllLines(Paths.get("rsyslog/output/recieved.log"), StandardCharsets.UTF_8).size();
+		} while (actual < expected);
+		if (expected < actual)
+			throw new AssertionError("Expected '" + expected + "' lines but got '" + actual + "'");
 	}
 
 	private static String generateRand(int length) {
 		return new BigInteger(length * 5, random).toString(32);
 	}
 
-	private static boolean hasRolled(Path output, final String fileName, int index, int times) throws Exception {
-		if (index % 10000 != 0)
-			return false;
-
+	private static boolean hasRolled(Path output, final String fileName, int times) throws Exception {
 		String[] listed = output.toFile().list(new FilenameFilter() {
 			public boolean accept(File dir, String name) {
 				return (name.startsWith(fileName)) ? true : false;
